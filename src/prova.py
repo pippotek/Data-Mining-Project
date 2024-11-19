41698c1e092e2d44cadc08b981fdc194cc2c12db
pippotek <filippo.costamagna@studenti.unitn.it>
2024-11-19 10:39:43 +0100

    first setup

---
+++ src/prova.py
@@ -0,0 +1,87 @@
+import requests
+import json
+import time
+from setup import load_config
+
+# Load the configuration
+config = load_config("config.yaml")
+
+# Access specific values
+if config:
+    api_key = config.get('api_key', None)    
+
+# Set the headers for authorization
+headers = {'x-api-key': api_key}
+
+# URL for the base API call (replace 'base_url' with the actual API endpoint)
+base_url = 'https://api.newscatcherapi.com/v2/search'
+
+# List of parameters to make multiple queries
+params = [
+    {
+        'q': "Meloni",
+        'lang': "it",
+        'to_rank': 10000,
+        'page_size': 100,
+        'page': 1,
+    },
+    {
+        'q': "Meloni",
+        'lang': "it",
+        'to_rank': 10000,
+        'page_size': 100,
+        'page': 2,
+    },
+    {
+        'q': "Meloni",
+        'lang': "it",
+        'to_rank': 10000,
+        'page_size': 100,
+        'page': 3,
+    },
+]
+
+# Variable to store all found news articles
+all_news_articles_mp = []
+
+# Loop through each set of parameters to fetch results page by page
+for separated_param in params:
+    print(f'Query in use => {str(separated_param)}')
+
+    # Wait for 1 second between each call to avoid hitting API rate limits
+    time.sleep(1)
+
+    # GET request to fetch the articles based on the current parameters
+    response = requests.get(base_url, headers=headers, params=separated_param)
+    results = response.json()
+
+    if response.status_code == 200:
+        print(f'Done for page number => {separated_param["page"]}')
+
+        # If there are no articles in the response, continue to the next set of parameters
+        if 'articles' not in results or not results['articles']:
+            print(f"No articles found for page number => {separated_param['page']}")
+            continue
+
+        # Adding your parameters to each result to be able to explore afterward
+        for i in results['articles']:
+            i['used_params'] = str(separated_param)
+
+        # Storing all found articles
+        all_news_articles_mp.extend(results['articles'])
+
+        print(f'Number of articles extracted for page {separated_param["page"]}: {len(results["articles"])}')
+
+    else:
+        # Print the error response if the API call fails
+        print(results)
+        print(f'ERROR: API call failed for page number => {separated_param["page"]}')
+
+# Print the total number of extracted articles
+print(f'Total number of extracted articles => {str(len(all_news_articles_mp))}')
+
+# Save the content of all extracted articles to a JSON file
+with open('ita_articles.json', 'w') as json_file:
+    json.dump(all_news_articles_mp, json_file, indent=4)
+
+print("Data has been saved to bitcoin_articles.json")
---
+++ src/provamongo.py
@@ -0,0 +1,42 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import json
+
+from setup import load_config
+
+# Load the configuration
+config = load_config("src/config.yaml")
+
+# Access specific values
+if config:
+    uri = config.get('db_connection_string', None)
+    print(f"Database Connection String: {uri}")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+with open('/home/filippo/Desktop/Uni/Data-Mining-Project/bitcoin_articles.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)  # Load JSON data as a Python list
+
+# Filter the data to keep only the specified fields
+filtered_data = [
+    {
+        "_id": article["_id"],  # Renaming `id` to `_id` to use as MongoDB's primary key if it exists
+        "title": article.get("title"),
+        "author": article.get("author"),
+        "link": article.get("link"),
+        "summary": article.get("summary"),
+        "excerpt": article.get("excerpt")
+    }
+    for article in data
+]
+
+# Connect to MongoDB  # Use your MongoDB URI if it's different
+db = client['NEWS']  # Replace with your database name
+
+# Insert data into the 'articles' collection
+articles_collection = db['articles']
+result = articles_collection.insert_many(filtered_data)  # Insert the list of articles
+
+# Print out the inserted IDs to verify
+print("Inserted document IDs:", result.inserted_ids)
